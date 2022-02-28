92a22191acd4b87ee136133ba824e14f17081712
eileenyq <eileenyuantw@gmail.com>
2022-02-28 10:35:08 -0500

    Initial commit

---
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //User can tap a button to change text color
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Change text color of label
+                ((TextView)findViewById(R.id.text)).setTextColor(getResources().getColor(R.color.yellow));
+                }
+        });
+    }
+}
