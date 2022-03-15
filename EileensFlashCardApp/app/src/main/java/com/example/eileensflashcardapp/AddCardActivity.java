a4b7c2ea95e7f60a099ec1bf678370e923615178
eileenyq <eileenyuantw@gmail.com>
2022-03-14 23:03:39 -0400

    Lab 2

---
+++ EileensFlashCardApp/app/src/main/java/com/example/eileensflashcardapp/AddCardActivity.java
@@ -0,0 +1,41 @@
+package com.example.eileensflashcardapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class AddCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_card);
+
+
+        findViewById(R.id.saveBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent data = new Intent(); // create a new Intent, this is where we will put our data
+                String questionString = ((EditText) findViewById(R.id.questionTextField)).getText().toString();
+                String answerString = ((EditText) findViewById(R.id.answerTextField)).getText().toString();
+                data.putExtra("string1", questionString); // puts one string into the Intent, with the key as 'string1'
+                data.putExtra("string2", answerString); // puts another string into the Intent, with the key as 'string2
+                setResult(RESULT_OK, data); // set result code and bundle data for response
+                finish(); // closes this activity and pass data to the original activity that launched this activity
+            }
+        });
+        findViewById(R.id.cancelBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+
+}
--- EileensFlashCardApp/app/src/main/java/com/example/eileensflashcardapp/MainActivity.java
+++ EileensFlashCardApp/app/src/main/java/com/example/eileensflashcardapp/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.eileensflashcardapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,13 +22,41 @@ public class MainActivity extends AppCompatActivity {
         TextView flashcardAnswer = findViewById(R.id.flashcard_answer);
         flashcardQuestion.setVisibility(View.VISIBLE);
         flashcardAnswer.setVisibility(View.INVISIBLE);
-        flashcardQuestion.setOnClickListener(new View.OnClickListener() {
 
+        flashcardQuestion.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick (View v) {
+            public void onClick(View v) {
                 flashcardQuestion.setVisibility(View.INVISIBLE);
                 flashcardAnswer.setVisibility(View.VISIBLE);
             }
         });
+
+        findViewById(R.id.myBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+                MainActivity.this.startActivityForResult(intent, 100);
+            }
+        });
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100) { // this 100 needs to match the 100 we used when we called startActivityForResult!
+            if(data !=null){
+                String string1 = data.getExtras().getString("string1"); // 'string1' needs to match the key we used when we put the string in the Intent
+                String string2 = data.getExtras().getString("string2");
+                TextView question = findViewById(R.id.flashcard_question);
+                question.setText(string1);
+                TextView answer = findViewById(R.id.flashcard_answer);
+                answer.setText(string2);
+            }
+            Snackbar.make(findViewById(R.id.flashcard_question),
+                    "Returned to main page",
+                    Snackbar.LENGTH_SHORT)
+                    .show();
+        }
+    }
+
+
 }
